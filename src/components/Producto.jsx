52ceabbc012d655bfacb47908512202f7ad440f2
maumainsaw <maumainsaw@gmail.com>
2020-08-04 22:03:17 -0500

    Evento Simple en react

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 const Producto = ({producto}) => {
 
     const {nombre,precio, id}= producto;
+    //agregar producto al carrito
+    const seleccionarProducto = id=>{
+        console.log('Comprando...'+id)
+    }
     return ( 
         <div>
             <h2>{nombre}</h2>
             <h3>{precio}</h3>
+            <button type='button' onClick={()=>seleccionarProducto(id)} >
+                Comprar
+            </button>
         </div>
      );
 }
